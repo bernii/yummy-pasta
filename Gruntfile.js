17fcfd906366aff5779ee30bd7993ad11903e28e
jonahss <jonahss@gmail.com>
2014-01-23 12:23:16 -0800

    removed opera for being buggy >.<

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,14 +13,6 @@ module.exports = function(grunt) {
         browserName: "internet explorer",
         platform: "WIN8",
         version: "10"
-    }, {
-        browserName: "internet explorer",
-        platform: "VISTA",
-        version: "9"
-    }, {
-        browserName: "opera",
-        platform: "Windows 2008",
-        version: "12"
     }];
 
     grunt.initConfig({
